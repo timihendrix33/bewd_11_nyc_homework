c5bccf44d93b4a7583ec02f230d98968e37b35bf
Matt Thees <mthees7@gmail.com>
2014-06-30 13:43:45 -0400

    feature addition to Homework: guesses

--- _Matt_Thees/Random_Number/random_number.rb
+++ _Matt_Thees/Random_Number/random_number.rb
@@ -1,26 +1,43 @@
 # random number game - user has to guess a random number between 1 and 9 and the
 # game must tell them if they are right or wrong
 
-def check_number (input, number)
+def check_number (number, guesses_left)
   # while loop tells runs the program until you guess correctly
-  while input != number do
-    if input > number
-      puts "Too High!"
-    elsif input < number
-      puts "Too Low!"
-    end
+  while guesses_left > 0 do
+
+    puts "Enter a guess:"
+
     # get new input if previous input is incorrect
     input = gets.chomp.to_i
-  end
-  puts "Winner!!! You got it!"
-end
 
-puts "Random Number Game! Guess a number between 1 and 9."
+    if input != number
 
-# get initial input
-input = gets.chomp.to_i
+      if input > number
+        puts "Too High!"
+      elsif input < number
+        puts "Too Low!"
+      end
+
+      guesses_left -= 1
+      puts "You have #{guesses_left} guess(es) remaining."
+
+    elsif input == number
+      puts "YOU GOT IT!!"
+      guesses_left = 0
+    end # input != number
+
+  end # guesses_left > 0
+  puts "Game Over."
+  # puts "You got it!"
+end # check_number
+
+puts "Random Number Game! Guess a number between 1 and 10. You have only 3 guesses to do so."
+
 # create array of numbers for program to sample
-number_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+number_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# program chooses the number
 number = number_array.sample
+# guesses
+guesses_left = 3
 
-check_number(input, number)
+check_number(number, guesses_left)
