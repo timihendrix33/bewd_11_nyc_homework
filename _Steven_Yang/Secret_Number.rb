db86599f906714970490f6ca8811f879cddc933c
zhikyang <zhikyang@gmail.com>
2014-06-29 20:23:27 -0400

    Added While Loop

--- _Steven_Yang/Secret_Number.rb
+++ _Steven_Yang/Secret_Number.rb
@@ -13,9 +13,15 @@ def IsguessCorrect(guess)
 			
 end
 
-puts "Please take you guess at my secret_number"
-guess = user_input
-
-answer = IsguessCorrect(guess.to_i)
-puts "is your guess correct? #{answer}"
-
+count = 3 
+while count>0
+	puts "You have #{count} time(s) to guess..."
+	puts "Please take you guess at my secret_number"
+	guess = gets
+	answer = IsguessCorrect(guess.to_i)
+	puts "is your guess correct? #{answer}"
+	if answer == 'YES'
+		break
+	end
+	count = count - 1 
+end
