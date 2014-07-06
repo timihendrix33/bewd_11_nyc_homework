b13d40c740a57fda3f8156693b3d9fcd3f49f509
Jenny de Campos <jcampos@squarespace.com>
2014-07-06 13:02:42 -0400

    code demo homework- Jenny Campos

---
+++ _Jenny_de/code_demo_apartment.rb
@@ -0,0 +1,15 @@
+#Explaining classes, objects, attr_accessor & load paths. 
+#TIME: 30 min
+
+
+class Apartment
+	attr_accessor :name, :rent, :sqft, :num_bedrooms, :num_bathrooms, :renters
+
+	def initialize(name, sqft, num_bedrooms, num_bathrooms)
+		@name = name
+		@sqft = sqft
+		@num_bedrooms = num_bedrooms
+		@num_bathrooms
+end
+
+
