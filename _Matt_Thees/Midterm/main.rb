0507ba7d9afad671e0395eb76b15f03e93cd3cc0
Matt Thees <mthees7@gmail.com>
2014-07-10 11:47:12 -0400

    adding the skeleton of folders and files for Midterm

---
+++ _Matt_Thees/Midterm/main.rb
@@ -0,0 +1,20 @@
+# require_relative 'lib/building'
+# require_relative 'lib/apartment'
+# require_relative 'lib/person'
+
+# def create_apartment
+#   puts "-----------New Apartment-----------"
+#   puts "What is the apartment number / name?"
+#   name = gets.strip
+
+#   puts "How many square feet is apartment #{name}?"
+#   apt_sqft = gets.to_i
+
+#   puts "How many bedrooms does apartment #{name} have?"
+#   apt_bedrooms = gets.to_i
+
+#   puts "How many bathrooms does apartment #{name} have?"
+#   apt_bathrooms = gets.to_i
+
+#   Apartment.new(name, apt_sqft, apt_bedrooms, apt_bathrooms)
+# end
