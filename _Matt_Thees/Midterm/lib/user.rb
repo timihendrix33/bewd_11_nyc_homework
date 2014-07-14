b7f334241f3e40fda4f66e995d9e98dae8c6057c
Matt Thees <mthees7@gmail.com>
2014-07-14 16:24:59 -0400

    updating midterm

---
+++ _Matt_Thees/Midterm/lib/user.rb
@@ -0,0 +1,30 @@
+# #Apartment class.
+# class Apartment
+#   attr_accessor :name, :sqft, :num_bedrooms, :num_bathrooms, :renter, :rent
+
+#   def initialize(name, sqft, num_bedrooms, num_bathrooms)
+#     @name = name
+#     @sqft = sqft
+#     @num_bedrooms = num_bedrooms
+#     @num_bathrooms = num_bathrooms
+#     @renter = nil
+#     @rent = nil
+#   end
+
+#   def is_occupied?
+#     @renter != nil
+#   end
+
+#   def to_s
+#     "Apartment: #{@name} \n sqft: #{@sqft} \t Bedrooms: #{@num_bedrooms} \t Bathrooms: #{@num_bathrooms}"
+#   end
+# end
+
+#user class
+class User
+  attr_accessor :user_name
+
+  def initialize(user_name)
+    @user_name = user_name
+  end
+end
