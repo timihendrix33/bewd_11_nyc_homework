201fab19148389d4e0ad64a3324536fccd417699
Timothy.Stevenson <t.stevenson33@gmail.com>
2014-07-14 17:02:03 -0400

    progress on midterm

--- _Tim_Stevenson/midterm/lib/recipient.rb
+++ _Tim_Stevenson/midterm/lib/recipient.rb
@@ -1,5 +1,6 @@
 #Recipient Class
 class Recipient
+
 	attr_accessor :name, :description
 
 	def initialize(name, description)
@@ -11,9 +12,24 @@ class Recipient
 		"You're shopping for #{@name}, in the #{@description} gift category."
 	end
 
-	def search_gifts |description|
+	def search_gifts
+		#This calls active listings under the category supplied by user. Need to figure out how to get only gift ideas> category 
+		etsy_api = JSON.load(RestClient.get("https://openapi.etsy.com/v2/listings/active?api_key=mgzshgbo2hgqvgsejc7pgdun&#{@description}"))
+	
 		puts "Would any of these interest #{name}? Enter (1), (2) or (3)"
 
-		etsy_api = JSON.load(RestClient.get("https://openapi.etsy.com/v2/categories/#{description}?api_key=mgzshgbo2hgqvgsejc7pgdun"))
-	end
+		@listing_titles = Array.new
+
+		
+		#List out first three gift ideas from the category and number them. 
+		#Next I'll get the user input for which one they like best (if any) and then get that item's url
+		1.upto(3) do |number|
+			etsy_api["results"].take(3).each do |listing|
+				@title = listing["title"]
+
+				puts "(#{number}) #{@title}"
+
+			end
+		end
+	end	
 end
--- _Tim_Stevenson/midterm/main.rb
+++ _Tim_Stevenson/midterm/main.rb
@@ -1,3 +1,4 @@
+#Trying to build an application that asks a gift recipient's name, what they like out of a list of gift categories and then serve up gift ideas in that category.
 
 require 'json'
 require 'rest-client'
@@ -6,12 +7,13 @@ require_relative 'lib/recipient.rb'
 
 def create_recipient
 
-	@description_list = ["For Couples", "For Him", "For Her", "Teens", "Kids & Baby"]
+	#currently supplying an array of categories on etsy. need to figure out how to get the categories under "gift ideas"
+	@description_list = ["Clothing", "toys", "furniture", "pets"]
 
 	puts "--------------New Gift Recipient---------------"
 	puts "Who are you shopping for?"
 	name = gets.strip
-	puts "Which of these best describes #{name}? \n\n #{@description_list}"
+	puts "Which of these does #{name} prefer? \n\n #{@description_list}"
 	description = gets.strip
 
 	Recipient.new(name, description)
@@ -23,7 +25,7 @@ puts "******************Welcome to Gift Picker!****************** \n \n \n"
 
 recipient = create_recipient
 
-puts 
+recipient.search_gifts
 
 
 
