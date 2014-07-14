8fc482b3272d851e407af7079668ff4d980bb5b4
Jenny de Campos <jcampos@squarespace.com>
2014-07-13 23:02:01 -0400

    homework in progress

---
+++ _Jenny_de/cat.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+
+
+puts "do you like cats? (Yes or No)"
+cats = gets.chomp
+ if cats == 'Yes'
+# Open http://placekitten.com/ for reading on line 4!
+kittens = open('http://placekitten.com/')
+body = kittens.read[559, 441]
+
+# Add your puts statement below!
+puts body
+puts "See the computer Cat Attack!"
+
+elsif cats == "No"
+	puts "I like dogs too"
+	puts "..but the dog placeholder site wasn't working, or anything else! So you just get text for now instead"
+
+else cats != "No" || "Yes" 
+ puts "There are only Dog or Cat people allowed to run this program! Sorry!"
+end
+ 
+
