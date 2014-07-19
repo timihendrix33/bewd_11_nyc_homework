1e14e7b0ffcd7235e075856e430828b3ee5b38ad
Timothy.Stevenson <t.stevenson33@gmail.com>
2014-07-19 14:29:45 -0400

    got movies info to print to index

--- _Tim_Stevenson/Movie_App/app/controllers/movies_controller.rb
+++ _Tim_Stevenson/Movie_App/app/controllers/movies_controller.rb
@@ -1,19 +1,7 @@
 class MoviesController < ApplicationController
-	attr_accessor :name, :description, :year_released
-
-	# def initialize(name, description, year_released)
-	# 	@name = name
-	# 	@description = description
-	# 	@year = year_released
-	# end
+	attr_accessor :title, :description, :year_released
 
   def index
-
-
-	@movies = Movie.all
-
-		# @movies = "#{@name} #{@description} #{@year}"
 	
-
   end
 end
