2c7fa13639f6d400208115649b83f3803a9b0a8b
Timothy.Stevenson <t.stevenson33@gmail.com>
2014-07-19 13:52:43 -0400

    working on movie app

--- _Tim_Stevenson/Movie_App/app/controllers/movies_controller.rb
+++ _Tim_Stevenson/Movie_App/app/controllers/movies_controller.rb
@@ -1,4 +1,19 @@
 class MoviesController < ApplicationController
+	attr_accessor :name, :description, :year_released
+
+	# def initialize(name, description, year_released)
+	# 	@name = name
+	# 	@description = description
+	# 	@year = year_released
+	# end
+
   def index
+
+
+	@movies = Movie.all
+
+		# @movies = "#{@name} #{@description} #{@year}"
+	
+
   end
 end
--- _Tim_Stevenson/Movie_App/app/models/movie.rb
+++ _Tim_Stevenson/Movie_App/app/models/movie.rb
@@ -1,2 +1,16 @@
 class Movie < ActiveRecord::Base
+	# attr_accessor :name, :description, :year_released
+
+	# def initialize(name, description, year_released)
+	# 	@name = name
+	# 	@description = description
+	# 	@year = year_released
+	# end
+
+	# # @movies = Movie.all
+
+	# def to_s
+
+	# 	@movies = "#{@name} #{@description} #{@year}"
+	# end
 end
