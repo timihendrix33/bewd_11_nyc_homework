a3fe0af459c88ef234e15c39b15ce510cc30e0eb
aditishekar <aditishekar@gmail.com>
2014-07-21 19:21:34 -0400

    Commiting movie app homework - I may have mislabeled my table; tried to correct it..

---
+++ _Aditi_Shekar/Movie_App/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieApp::Application.config.secret_key_base = '6b31243543d5602752a14cc8d9f1f57558b59bf4804dbf38f2cf1c15078b24aff2fab34b34c7b0ad43cb2741f0f02975a546b5ba0965054ae3aa2c1a31feebf1'
