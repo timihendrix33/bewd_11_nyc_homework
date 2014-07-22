77d12ad17b615cd2f4c736e1bd6dbe0feb2fbc45
Timothy.Stevenson <t.stevenson33@gmail.com>
2014-07-21 20:52:47 -0400

    added search form

--- _Tim_Stevenson/Movie_App/app/controllers/movies_controller.rb
+++ _Tim_Stevenson/Movie_App/app/controllers/movies_controller.rb
@@ -2,6 +2,6 @@ class MoviesController < ApplicationController
 	attr_accessor :title, :description, :year_released
 
   def index
-	
+	@movies = Movie.search_for(params[:q])
   end
 end
--- _Tim_Stevenson/Movie_App/app/models/movie.rb
+++ _Tim_Stevenson/Movie_App/app/models/movie.rb
@@ -1,16 +1,7 @@
 class Movie < ActiveRecord::Base
-	# attr_accessor :name, :description, :year_released
 
-	# def initialize(name, description, year_released)
-	# 	@name = name
-	# 	@description = description
-	# 	@year = year_released
-	# end
+  def self.search_for(query)
+    Movie.where('title LIKE :query OR description LIKE :query', query: "%#{query}%")
+  end
 
-	# # @movies = Movie.all
-
-	# def to_s
-
-	# 	@movies = "#{@name} #{@description} #{@year}"
-	# end
 end
