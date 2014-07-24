5c9cd9009dc1480f59b17718dbbbee626869ccf4
Timothy.Stevenson <t.stevenson33@gmail.com>
2014-07-23 20:04:53 -0400

    adding change to movie.rb

--- _Tim_Stevenson/Movie_App/app/models/movie.rb
+++ _Tim_Stevenson/Movie_App/app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
 
   def self.search_for(query)
-    Movie.where('title LIKE :query OR description LIKE :query', query: "%#{query}%")
+    Movie.where('title LIKE :query OR description LIKE :query OR year_released LIKE :query', query: "%#{query}%")
   end
 
 end
