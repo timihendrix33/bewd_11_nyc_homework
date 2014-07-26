bee88fe0ac49f6474dbc9c6ff9c7e172cbd8af0d
timihendrix33 <t.stevenson33@gmail.com>
2014-07-26 19:47:08 -0400

    working on Movie App hw

--- _Tim_Stevenson/Movie_App/app/controllers/movies_controller.rb
+++ _Tim_Stevenson/Movie_App/app/controllers/movies_controller.rb
@@ -4,4 +4,30 @@ class MoviesController < ApplicationController
   def index
 	@movies = Movie.search_for(params[:q])
   end
+
+  def show
+  	load_movie
+  end
+
+  def new
+  	@movie = Movie.new
+  end
+
+  def create
+  	@movie = Movie.new(safe_movie_params)
+  	if @movie.save
+  		redirect_to movie_path(@movie)
+  	else
+  		render 'new'
+  	end
+  end
+
+  private 
+  def safe_movie_params
+    params.require('movie').permit(:title, :description, :year_released)
+  end  
+
+  def load_movie
+  	@movie = Movie.find_by id: params[:id]
+  end
 end
--- _Tim_Stevenson/Movie_App/app/models/movie.rb
+++ _Tim_Stevenson/Movie_App/app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ActiveRecord::Base
+  validates :title, :description, presence: true
+  validates :description, length: { minimum: 10 }
+  validates :year_released, numericality: {greater_than: 0}
+  validates :year_released, length: {is: 4}
 
   def self.search_for(query)
     Movie.where('title LIKE :query OR description LIKE :query OR year_released LIKE :query', query: "%#{query}%")
