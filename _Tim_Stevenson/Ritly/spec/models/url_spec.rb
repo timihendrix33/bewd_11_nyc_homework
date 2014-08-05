4efe7f64f5dd5796add6f3b9c3a673eb46eeb5f5
Timothy.Stevenson <t.stevenson33@gmail.com>
2014-08-05 12:16:49 -0400

    working on TDD for ritly

--- _Tim_Stevenson/Ritly/spec/models/url_spec.rb
+++ _Tim_Stevenson/Ritly/spec/models/url_spec.rb
@@ -1,8 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Url, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  context
+  
+  context "add url" do
+  	before(:each) do
+  		url = Url.new_with_hash(link: "http://google.com")
+  		url.save
+  	end
+
+  	let(:url) {Url.find_by link: "http://google.com"}
+
+  	it "creates url" do
+  		expect(Url.count).to eq 1
+  	end
+
+  	it "gives url hash" do
+  		url = Url.first
+  		expect(url.hash_code.size).to eq 8 
+  	end
+ 
+
+
+  end
 
 
 end
